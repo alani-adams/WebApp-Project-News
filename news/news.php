92fb58bda57609876b302d3399968589a2f9dd93
gideonluck <gbl12a@acu.edu>
2017-02-05 22:11:43 -0600

    Add files via upload

--- news/news.php
+++ news/news.php
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `stories` (
 
 
 INSERT INTO `stories` (`id`, `title`, `date1`, `story`, `approved`, `submitted_by`, `photo`) VALUES
-(1, 'Test story', 'Oct 1, 2017', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis diam at erat auctor luctus. Donec lobortis mattis metus, ac lobortis tellus aliquet et. Nullam mattis dictum elit, vel auctor enim sagittis a. Aenean at odio rhoncus, volutpat felis et, vestibulum nulla. Curabitur vulputate consectetur massa, eget imperdiet orci vulputate elementum. Pellentesque imperdiet feugiat odio, eget aliquet ligula faucibus et. Cras at enim ut dolor lobortis consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed a est rutrum, dictum felis eget, gravida dui.', 1, 'example', 'image1.gif');
+(1, 'Test story', '01-10-2017', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis diam at erat auctor luctus. Donec lobortis mattis metus, ac lobortis tellus aliquet et. Nullam mattis dictum elit, vel auctor enim sagittis a. Aenean at odio rhoncus, volutpat felis et, vestibulum nulla. Curabitur vulputate consectetur massa, eget imperdiet orci vulputate elementum. Pellentesque imperdiet feugiat odio, eget aliquet ligula faucibus et. Cras at enim ut dolor lobortis consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed a est rutrum, dictum felis eget, gravida dui.', 1, 'example', 'image1.gif');
 
 */
 -->
@@ -32,7 +32,7 @@ INSERT INTO `stories` (`id`, `title`, `date1`, `story`, `approved`, `submitted_b
 	</head>
 	<body>
 
-		<h1>I <3 NEWS!</h1>
+		<h1>I <3 NEWS! </h1>
 		<?php
 			$link =  mysql_connect("localhost","root","");
 
@@ -50,14 +50,17 @@ INSERT INTO `stories` (`id`, `title`, `date1`, `story`, `approved`, `submitted_b
 			$sql = "SELECT id, title, date1, story, approved, submitted_by, photo FROM stories";
 
 			$result = mysql_query("select * from stories");;
-
+			$dir = 'C:/xampp/htdocs/news/uploads';
 			if (mysql_num_rows($result) > 0) 
 			{
      			// output data of each row
      			while($row = mysql_fetch_assoc($result)) {
      				if($row["approved"] == 1) {
-         				echo $row["title"]. "<br>" . "By " . $row["submitted_by"]. "<br>" . $row["date1"] . "<br>" . $row["story"] ."<br/>". "<br/>";
+         				echo "<br/>". "<br/>" . $row["title"]. "<br>" . "By " . $row["submitted_by"]. "<br>" . $row["date1"] . "<br>" . $row["story"] ."<br/>". "<br/>";
      				}
+     				//output the picture with each story
+     				echo '<img src="', $dir, '/', $row["photo"], '" alt="', $row["photo"], '" />';
+
      			}
 			} else {
      			echo "There are no stories!";
